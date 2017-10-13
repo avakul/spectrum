4990576c03c477457528d7a9bc87583158455de2
AV038529 <amaresh.vakul@cerner.com>
2017-10-13 15:50:35 -0400

    configstore - initial
    Initial code set.

---
+++ configstore/src/main/java/com/spectrum/configstore/ConfigstoreApplication.java
@@ -0,0 +1,12 @@
+package com.spectrum.configstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigstoreApplication.class, args);
+	}
+}
